01e2e0d7f2d296677a2282fcb9c7910ab405d1ec
emision <zavrazhnev.k@gmail.com>
2017-09-26 23:04:40 +0300

    resolve form block

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -814,4 +814,11 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 3
 });
+    $('.comment__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
